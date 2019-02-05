490ecd83ecdc5b14ddedc4e9edd64e3137626bde
etnpce <etnpce@gmail.com>
2019-02-05 15:05:23 -0700

    Add Info for Ethan Peace

--- group.c
+++ group.c
@@ -15,7 +15,16 @@ void Jimmy(void){
 }
 
 void Ethan(void){
-
+  printf(
+"\
+*****Ethan Peace****\n\
+Sophomore? maybe junior\n\
+Combined Comp-Math\n\
+\n\
+Likes Video games, youtube, and reading Tech blogs\n\
+Military Child, Born in Britain, moved frequently.\
+"
+  );
 }
 
 void Branden(void){
