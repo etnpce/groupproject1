4c3b7df95e6a538237137235ec56726ff67af715
ShelbyMax <44272479+ShelbyMax@users.noreply.github.com>
2019-02-05 15:01:05 -0700

    Add files via upload

---
+++ group.c
@@ -0,0 +1,44 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+void Shelby(void){
+
+}
+
+void Kody(void){
+}
+
+void Jimmy(void){
+
+}
+
+void Ethan(void){
+
+}
+
+void Branden(void){
+
+}
+
+
+int main(void){
+
+
+  printf("********************\n");
+  printf("   Group: Mangos\n");
+  printf("   Group Members\n");
+  printf("Jimmy\nShelby\nKody\nEthan\nBranden\n");
+  
+
+
+
+
+
+
+
+
+
+
+}
